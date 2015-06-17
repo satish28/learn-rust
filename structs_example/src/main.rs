958332d37b448ccd152c80eaeb498bc8294bfd98
satish28 <satish2888@gmail.com>
2015-06-17 20:06:42 +0530

    Structs example--Finding slope of line given coordinates

---
+++ structs_example/src/main.rs
@@ -0,0 +1,20 @@
+//slope of a line given the coordinates of the line
+
+struct Point {
+    x: f32,
+    y: f32,
+    }
+
+fn slope(a: Point,  b: Point){
+    let ans  = (a.y - b.y) / (a.x - b.x);
+    println!("The slope of the given line is {}", ans);
+}
+
+fn main(){
+ 
+    let a = Point{x : 10.0, y : 20.0};
+    let b = Point{x : 40.0, y : 5.0};
+
+    slope(a, b);
+    
+}
