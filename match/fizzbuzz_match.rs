b106715a48b49fca8e8ff1fab74409121b4902ca
dinesh <dsdiscoverdinesh@gmail.com>
2015-06-20 00:03:26 +0530

    Fix indentation

--- match/fizzbuzz_match.rs
+++ match/fizzbuzz_match.rs
@@ -1,14 +1,12 @@
-//fizzbuzz program using match
+// Fizzbuzz program using match
 
-fn main(){
-    for x in 1..101{
-        match x{
+fn main() {
+    for x in 1..101 {
+        match x {
             _ if x % 15 == 0 => println!("FizzBuzz"),
             _ if x % 5 == 0 => println!("Fizz"),
             _ if x % 3 == 0 =>  println!("Buzz"),
             _ => println!("{}", x),
-                
-            }
         }
     }
-        
+}
