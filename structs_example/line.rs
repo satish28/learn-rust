f34d288172ff4589ae2bc62f915d2d5f4a09301b
satish28 <satish2888@gmail.com>
2015-06-20 01:58:48 +0530

    Add MidPoint function to structs line example

--- structs_example/line.rs
+++ structs_example/line.rs
@@ -1,20 +1,32 @@
-//slope of a line given the coordinates of the line
-
 struct Point {
     x: f32,
     y: f32,
     }
 
-fn slope(a: Point,  b: Point){
+//calculate slope of a line given its coordinates
+fn slope(a: &Point,  b: &Point) -> f32{
     let ans  = (a.y - b.y) / (a.x - b.x);
-    println!("The slope of the given line is {}", ans);
+    return ans;
 }
+
+//Calculate the mid-point of a line
+fn midpoint(a :&Point, b: &Point) -> Point{
+    let mid = Point{x: (a.x + b.x)/2.0, y: (a.y + b.y)/2.0};
+    return mid;
+    }
 
 fn main(){
  
     // coordinates of a line
     let a = Point{x : 10.0, y : 20.0};
     let b = Point{x : 40.0, y : 5.0};
+    
+    println!("Slope of the given line is {}", slope(&a, &b));
+    
+    let mid = midpoint(&a, &b);
 
-    slope(a, b);
+    //Destructuring a struct data type using match
+    match mid{
+        Point {x: mid_x, y: mid_y} => println!("the mid point of the given line is ({}, {})", mid_x, mid_y),
+        }
 }
